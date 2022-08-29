f57a3c05e44d2ac48e3c6e91644ebda51b9d8d4b
Hoan Minh Hoang <hoan.hoang@asnet.com.vn>
2022-08-29 15:15:50 +0700

    feat: #69 - Create product model

---
+++ practice-two/src/models/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  src?: string;
+  quantity?: number;
+  weight?: number;
+  dimensions?: string;
+  colors?: string;
+  material?: string;
+  description?: string;
+}
