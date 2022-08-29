713c1d38a8a7c07ac5adf0edbce01fbd212325b8
Hoan Minh Hoang <hoan.hoang@asnet.com.vn>
2022-08-29 16:38:03 +0700

    update: #70 - config react router

--- practice-two/src/main.tsx
+++ practice-two/src/main.tsx
@@ -1,10 +1,18 @@
+// Libs
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Components
 import App from './App';
+
+// Contexts
 import { ProductProvider } from 'contexts/ProductContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ProductProvider>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ProductProvider>,
 );
