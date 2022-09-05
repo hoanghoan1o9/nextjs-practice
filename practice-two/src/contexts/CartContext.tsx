e01ae1e974b0323014475480121bbb9a4e956b36
Hoan Minh Hoang <hoan.hoang@asnet.com.vn>
2022-09-05 17:56:58 +0700

    update: #74 - Implement deleteCart, updateCart function

--- practice-two/src/contexts/CartContext.tsx
+++ practice-two/src/contexts/CartContext.tsx
@@ -15,6 +15,8 @@ import { Cart } from '../models/cart';
 interface CartContextValue extends CartState {
   getCartList: () => void;
   addCart: (cart: Cart) => void;
+  deleteCart: (id: string, index: number) => void;
+  updateCart: (id: string, cart: Cart) => void;
 }
 
 interface CartProviderProps {
@@ -25,6 +27,8 @@ export const CartContext = createContext<CartContextValue>({
   ...initialCartState,
   getCartList: () => Promise<Cart[]>,
   addCart: () => Promise<Cart>,
+  deleteCart: () => Promise<Cart>,
+  updateCart: () => Promise<void>,
 });
 
 export const CartProvider = ({ children }: CartProviderProps) => {
@@ -53,9 +57,34 @@ export const CartProvider = ({ children }: CartProviderProps) => {
       },
     });
   };
-  
+
+  const deleteCart = async (id: string, index: number): Promise<void> => {
+    await CartServices.deleteCart(id);
+
+    dispatch({
+      type: CARTS_ACTIONS.DELETE_CART,
+      payload: {
+        index,
+      },
+    });
+  };
+
+  const updateCart = async (id: string, data: Cart): Promise<void> => {
+    const response = await CartServices.patchCart(id, data);
+    console.log(response);
+    dispatch({
+      type: CARTS_ACTIONS.UPDATE_CART,
+      payload: {
+        id,
+        // cart: response,
+      },
+    });
+  };
+
   return (
-    <CartContext.Provider value={{ ...state, getCartList, addCart }}>
+    <CartContext.Provider
+      value={{ ...state, getCartList, deleteCart, addCart, updateCart }}
+    >
       {children}
     </CartContext.Provider>
   );
