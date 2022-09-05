3d7579e9174fb37efeab945b189358c79402fbbb
Hoan Minh Hoang <hoan.hoang@asnet.com.vn>
2022-09-05 17:53:22 +0700

    update: #70 - update api request

---
+++ practice-two/src/helpers/carts.ts
@@ -0,0 +1,35 @@
+// Models
+import { Cart } from 'models/cart';
+import { Product } from '../models/product';
+
+const generateCartList = (cartList: Cart[], products: Product[]) => {
+  let carts: Cart[] = [];
+  cartList.map((cart: Cart) => {
+    const product: Product | undefined = products.find(
+      (product: Product) => product.id === cart.id,
+    );
+    if (product)
+      carts = [
+        ...carts,
+        Object.assign(
+          cart,
+          {},
+          {
+            price: product.price,
+            title: product.title,
+            colors: product.colors,
+            src: product.src,
+          },
+        ),
+      ];
+  });
+
+  const totalPriceCart = carts.reduce((x, y) => x + y.price * y.quantity, 0);
+
+  return {
+    carts,
+    totalPriceCart,
+  };
+};
+
+export { generateCartList };
