33b95218db5d0f67a8110e163702e6afdd659ccd
Hoan Minh Hoang <hoan.hoang@asnet.com.vn>
2022-09-08 10:12:47 +0700

    update: #77 - Remove unuse constants

--- practice-two/src/constants/button.ts
+++ practice-two/src/constants/button.ts
@@ -2,17 +2,9 @@ export namespace BUTTON {
     
   export enum STYLES {
     PRIMARY = 'btn--primary',
-    SOLID_PRIMARY = 'btn--primary--solid',
-    SOLID_SECONDARY = 'btn--secondary--solid',
-    SOLID_WARNING = 'btn--warning--solid',
-    SOLID_DANGER = 'btn--danger--solid',
-    SOLID_SUCCESS = 'btn--success--solid',
     OUTLINE_PRIMARY = 'btn--primary--outline',
     SECONDARY = 'btn--secondary',
     OUTLINE_SECONDARY = 'btn--secondary--outline',
-    OUTLINE_WARNING = 'btn--warning--outline',
-    OUTLINE_DANGER = 'btn--danger--outline',
-    OUTLINE_SUCCESS = 'btn--success--outline',
     CLOSE = 'btn--close',
   }
 
