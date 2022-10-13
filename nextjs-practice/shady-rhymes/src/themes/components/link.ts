a9eec757ae715a33290fc7ddeb8cabe4817ebc81
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-13 16:32:23 +0700

    update: #134 - update variable value for nav link

--- nextjs-practice/shady-rhymes/src/themes/components/link.ts
+++ nextjs-practice/shady-rhymes/src/themes/components/link.ts
@@ -1,20 +1,20 @@
 export const Link = {
   sizes: {
     navBar: {
-      fontSize: 'tiny',
-      fontWeight: '500'
-    }
+      fontSize: 'default',
+      fontWeight: '400',
+    },
   },
 
   variants: {
     navBar: {
-      color: 'grey.600',
-      fontWeight: '500',
+      color: '#FFFFFF',
+      fontWeight: '400',
 
       _hover: {
         textDecoration: 'none',
-        color: 'primary'
-      }
-    }
-  }
+        color: '#f1d0cc',
+      },
+    },
+  },
 };
