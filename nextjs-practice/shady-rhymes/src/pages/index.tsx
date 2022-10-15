2cc5098348822c60b188878ecc02f0f05d26051c
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-15 11:47:01 +0700

    feat: #147 - Implement Seo component

--- nextjs-practice/shady-rhymes/src/pages/index.tsx
+++ nextjs-practice/shady-rhymes/src/pages/index.tsx
@@ -1,8 +1,19 @@
-import  Header  from '@components/common/Header';
+import { Seo } from '@components/common/Seo';
 import type { NextPage } from 'next';
 
+// FIXME: this field use for testing UI
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <Seo
+      props={{
+        title: 'NextJS | Shady Rhymes',
+        description: 'Shady Rhymes interiors design landing page',
+        url: 'https://metatags.io/',
+        thumbnailUrl:
+          'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png',
+      }}
+    />
+  );
 };
 
 export default Home;
