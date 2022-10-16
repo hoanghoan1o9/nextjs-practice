e5c8761c4e1cd7d371aa9d3a9007e9d291367aeb
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-16 14:29:07 +0700

    update: #137 - Remove unuse code, removed module css

--- nextjs-practice/shady-rhymes/src/components/Card/index.tsx
+++ nextjs-practice/shady-rhymes/src/components/Card/index.tsx
@@ -9,9 +9,6 @@ import { Text } from '@components/common/Text';
 // Constants
 import { APP_IMAGE_URLS } from '@constants/images';
 
-// Styles
-import styles from './Card.module.css';
-
 interface CardProps {
   href: APP_IMAGE_URLS;
   title: string;
@@ -20,13 +17,20 @@ interface CardProps {
 
 const CardItem = ({ href, title, description }: CardProps) => {
   return (
-    <Box className={styles['card-container']}>
-      <Image src={href} alt={title} width={60} height={60} />
-      <Text value={title} w="144px" pl="20px" my="10px" />
+    <Box
+      w="425px"
+      h="280px"
+      border="1px solid #fadcd9"
+      borderRadius="10px"
+      shadow="4px 2px 10px 2px #f7948933"
+    >
+      <Box mt="20px" ml="20px">
+        <Image src={href} alt={title} width={60} height={60} />
+      </Box>
+      <Text value={title} pl="20px" my="10px" />
       <Text value={description} w="385px" px="20px" mt="10px" mb="20px" />
       <Link href="/">
         <ChakraLink pl="20px" color="#f79489">
-          {' '}
           more info
         </ChakraLink>
       </Link>
