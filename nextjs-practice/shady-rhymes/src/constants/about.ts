109b5f6eb09cfedac5be33196d337d16e4b7c54c
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-17 10:40:03 +0700

    feat: #151 - Implement constant for about section

---
+++ nextjs-practice/shady-rhymes/src/constants/about.ts
@@ -0,0 +1,32 @@
+// Constants
+import { APP_IMAGE_URLS } from '@constants/images';
+
+export interface AboutItemProps {
+  title: string;
+  description: string;
+  href: APP_IMAGE_URLS;
+}
+
+export const ABOUT_ITEM: AboutItemProps[] = [
+  {
+    title: 'Professionalism',
+    description: 'We deliver the final work with great professional way.',
+    href: APP_IMAGE_URLS.CHECK_ICON,
+  },
+  {
+    title: 'Smooth workflow',
+    description:
+      'We provide the most easy smoth workflow of interior design.y.',
+    href: APP_IMAGE_URLS.ICON_DOUBLE,
+  },
+  {
+    title: 'Trusted Platform',
+    description: 'People trust us since we openend the company.',
+    href: APP_IMAGE_URLS.ICON_STICK,
+  },
+  {
+    title: 'Qualified employee',
+    description: 'Our employees are the best of the field from the city.',
+    href: APP_IMAGE_URLS.ICON_STAR,
+  },
+];
