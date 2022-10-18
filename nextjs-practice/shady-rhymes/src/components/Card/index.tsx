0c96a841ad92cd0f9665da392cfa9d479de51b9d
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-18 14:49:29 +0700

    update: #137 - Update common variable for card component

--- nextjs-practice/shady-rhymes/src/components/Card/index.tsx
+++ nextjs-practice/shady-rhymes/src/components/Card/index.tsx
@@ -20,9 +20,9 @@ const CardItem = ({ href, title, description }: CardProps) => {
     <Box
       w="425px"
       h="280px"
-      border="1px solid #fadcd9"
+      border="1px solid var(--chakra-borders-mainBorder)"
       borderRadius="10px"
-      shadow="4px 2px 10px 2px #f7948933"
+      shadow="4px 2px 10px 2px var(--chakra-shadows-mainShadow)"
     >
       <Box mt="20px" ml="20px">
         <Image src={href} alt={title} width={60} height={60} />
@@ -30,7 +30,7 @@ const CardItem = ({ href, title, description }: CardProps) => {
       <Text value={title} pl="20px" my="10px" />
       <Text value={description} w="385px" px="20px" mt="10px" mb="20px" />
       <Link href="/">
-        <ChakraLink pl="20px" color="#f79489">
+        <ChakraLink pl="20px" variant="normalLink">
           more info
         </ChakraLink>
       </Link>
