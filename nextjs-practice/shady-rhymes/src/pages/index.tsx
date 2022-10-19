9e2612bd24578d6bec29302d37b57d2ac079dbdb
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-19 10:28:15 +0700

    feat: #141 - Implement Home page and request data from server

--- nextjs-practice/shady-rhymes/src/pages/index.tsx
+++ nextjs-practice/shady-rhymes/src/pages/index.tsx
@@ -1,14 +1,68 @@
+// Libs
+import { useEffect } from 'react';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+
+// Components
 import { ProductListSection } from '@components/sections/productListSection';
 import { MainLayout } from '@components/layouts/mainLayout';
 import { Banner } from '@components/featured/Banner';
 import { AboutSection } from '@components/sections/aboutSection';
 import { InforSection } from '@components/sections/InfoSection';
 
-// Types
-import type { NextPage } from 'next';
+// Services
+import { ProductServices } from '@services/productService';
+
+// Context
+import { useProduct } from '@contexts/ProductContext';
+
+// Constants
+import { API_ENDPOINTS } from '@constants/clientApis';
+import { APP_ERRORS } from '@constants/errors';
+
+export const getStaticProps: GetStaticProps = async () => {
+  // For testing ISR flow
+  console.log('====Generate/ Re-generate Product List====');
+  try {
+    const responseProducts = await ProductServices.getProductList(
+      API_ENDPOINTS.PRODUCTS,
+    );
+
+    if (!responseProducts) {
+      throw new Error(APP_ERRORS.DEFAULT_ERROR_APIS);
+    }
+
+    return {
+      props: {
+        products: responseProducts,
+      },
+      // 10 seconds is for quickly testing.
+      revalidate: 10,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        props: {
+          error: error.message,
+        },
+        // 10 seconds is for quickly testing.
+        revalidate: 10,
+      };
+    }
+
+    return { props: {} };
+  }
+};
 
 // FIXME: this field use for testing UI
-const Home: NextPage = () => {
+export function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { setProductList } = useProduct();
+
+  useEffect(() => {
+    setProductList(products);
+  }, []);
+
   return (
     <MainLayout>
       <Banner />
@@ -17,6 +71,6 @@ const Home: NextPage = () => {
       <ProductListSection />
     </MainLayout>
   );
-};
+}
 
 export default Home;
