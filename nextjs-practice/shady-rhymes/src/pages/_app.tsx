cbafb8392384cefdfeade2417d0d32966d94e5b8
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-23 22:42:54 +0700

    update: #157 - Update protected page

--- nextjs-practice/shady-rhymes/src/pages/_app.tsx
+++ nextjs-practice/shady-rhymes/src/pages/_app.tsx
@@ -4,6 +4,11 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Script from 'next/script';
 
+import { useRouter } from 'next/router';
+import { ROUTES } from '@constants/router';
+import { getItem, IS_LOGIN } from '@helpers/localStorage';
+import { useEffect } from 'react';
+
 // Components
 import { LoadingIndicator } from '@components/common/LoadingIndicator';
 import { ErrorBoundary } from '@components/common/ErrorBoundary';
@@ -17,20 +22,33 @@ import { AppProvider } from '@contexts/AppProvider';
 // Styles
 import '@styles/main.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Script
-      strategy="lazyOnload"
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-    />
-    <Script strategy="lazyOnload">
-      {` 
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const isBrowser = typeof window !== 'undefined';
+
+  const router = useRouter();
+
+  const isLogin = isBrowser && getItem(IS_LOGIN);
+
+  useEffect(() => {
+    if (!isLogin) {
+      router.push(ROUTES.LOGIN);
+    }
+  }, [isLogin]);
+
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {` 
          window.dataLayer = window.dataLayer || [];
          function gtag(){dataLayer.push(arguments);}
          gtag('js', new Date());
          gtag('config', ${process.env.GOOGLE_ANALYTICS});
         `}
-    </Script>
+      </Script>
       <ChakraProvider theme={CHAKRA_THEME}>
         <Suspense fallback={<LoadingIndicator />}>
           <AppProvider>
@@ -40,7 +58,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
           </AppProvider>
         </Suspense>
       </ChakraProvider>
-  </>
-);
+    </>
+  );
+};
 
 export default MyApp;
