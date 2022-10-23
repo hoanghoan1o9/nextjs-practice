5c7bd93235a917f45e1851ea681da2adfbf12a7b
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-23 22:37:15 +0700

    update: #157 - Update  constants

--- nextjs-practice/shady-rhymes/src/constants/clientApis.ts
+++ nextjs-practice/shady-rhymes/src/constants/clientApis.ts
@@ -1,8 +1,17 @@
-export namespace API {
-    
-}
+export namespace API {}
 export const BASE_URL_API = process.env.BASE_API_URL;
 
 export const API_ENDPOINTS = {
   PRODUCTS: '/products',
 };
+
+export const METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+};
+
+export enum HEADERS {
+  'Content-Type' = 'application/json',
+}
