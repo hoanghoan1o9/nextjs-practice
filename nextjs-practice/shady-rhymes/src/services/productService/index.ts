500f45000f633ddc6f048667fb761b6a18345b1d
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-23 22:45:29 +0700

    update: #157 - update products services

--- nextjs-practice/shady-rhymes/src/services/productService/index.ts
+++ nextjs-practice/shady-rhymes/src/services/productService/index.ts
@@ -2,7 +2,12 @@
 import { Product } from '@models/Product';
 
 // Constants
-import { BASE_URL_API } from '@constants/clientApis';
+import {
+  BASE_URL_API,
+  METHODS,
+  HEADERS,
+  API_ENDPOINTS,
+} from '@constants/clientApis';
 
 export class ProductClient {
   private readonly baseApi: string;
@@ -25,6 +30,37 @@ export class ProductClient {
   getProduct = async (url: string, id: string): Promise<Product[]> => {
     const urlProduct = `${this.baseApi}${url}/${id}`;
     const response = await fetch(urlProduct);
+
+    return response.json();
+  };
+
+  addProduct = async (product: Product): Promise<Product> => {
+    const response = await fetch(this.baseApi, {
+      method: METHODS.POST,
+      headers: HEADERS,
+      body: JSON.stringify(product),
+    });
+
+    return response.json();
+  };
+
+  deleteProduct = async (id: number): Promise<boolean> => {
+    const url = `${this.baseApi}/${API_ENDPOINTS.PRODUCTS}/${id}`;
+    const response = await fetch(url, {
+      method: METHODS.DELETE,
+      headers: HEADERS,
+    });
+
+    return !!response.ok;
+  };
+
+  patchProduct = async (product: Product): Promise<Product> => {
+    const url = `${this.baseApi}/${API_ENDPOINTS.PRODUCTS}/${product.id}`;
+    const response = await fetch(url, {
+      method: METHODS.PATCH,
+      headers: HEADERS,
+      body: JSON.stringify(product),
+    });
 
     return response.json();
   };
