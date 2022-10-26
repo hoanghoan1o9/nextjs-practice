1c255a13126db77f228233211c2570ab88350036
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-27 01:00:24 +0700

    feat: #157 - implement unit test for text

--- nextjs-practice/shady-rhymes/src/components/common/Text/Text.test.tsx
+++ nextjs-practice/shady-rhymes/src/components/common/Text/Text.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 
 // Components
-import Text from './index';
+import { Text } from './index';
 
 const mockProps = {
   value: 'Text',
