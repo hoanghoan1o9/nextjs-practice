51897349b21093cbd401e16aa9a7bc47536be6ef
hoan.hoang@asnet.com.vn <hoan.hoang@asnet.com.vn>
2022-10-30 19:54:19 +0700

    update: #157 - update layout for product __app page

--- nextjs-practice/shady-rhymes/src/pages/_app.tsx
+++ nextjs-practice/shady-rhymes/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 // Libs
 import { Suspense } from 'react';
-import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { useRouter } from 'next/router';
@@ -8,10 +7,14 @@ import { ROUTES } from '@constants/router';
 import { getItem, IS_LOGIN } from '@helpers/localStorage';
 import { useEffect } from 'react';
 
+// Models
+import { AppPropsWithLayout } from '@models/common';
+
 // Components
 import { LoadingIndicator } from '@components/common/LoadingIndicator';
 import { ErrorBoundary } from '@components/common/ErrorBoundary';
 import { SEO } from '@components/common/SEO';
+import EmptyLayout from '@components/layouts/Empty';
 
 // Themes
 import { CHAKRA_THEME } from '@themes/chakra';
@@ -22,7 +25,7 @@ import { AppProvider } from '@contexts/AppProvider';
 // Styles
 import '@styles/main.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const isBrowser = typeof window !== 'undefined';
 
   const router = useRouter();
@@ -35,27 +38,29 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     }
   }, [isLogin]);
 
+  const Layout = Component.Layout ?? EmptyLayout;
+
   return (
-    <>
-      <ChakraProvider theme={CHAKRA_THEME}>
-        <Suspense fallback={<LoadingIndicator />}>
-          <AppProvider>
-            <ErrorBoundary>
-              <SEO
-                props={{
-                  title: 'NextJS | Shady Rhymes',
-                  description: 'Shady Rhymes interiors design landing page',
-                  url: 'https://shady-rhymes.vercel.app',
-                  thumbnailUrl:
-                    'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png',
-                }}
-              />
+    <ChakraProvider theme={CHAKRA_THEME}>
+      <Suspense fallback={<LoadingIndicator />}>
+        <AppProvider>
+          <ErrorBoundary>
+            <SEO
+              props={{
+                title: 'NextJS | Shady Rhymes',
+                description: 'Shady Rhymes interiors design landing page',
+                url: 'https://shady-rhymes.vercel.app',
+                thumbnailUrl:
+                  'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png',
+              }}
+            />
+            <Layout>
               <Component {...pageProps} />
-            </ErrorBoundary>
-          </AppProvider>
-        </Suspense>
-      </ChakraProvider>
-    </>
+            </Layout>
+          </ErrorBoundary>
+        </AppProvider>
+      </Suspense>
+    </ChakraProvider>
   );
 };
 
